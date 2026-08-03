7f602d1f7e0a863ef604b7dfd23670cd12d6a9f3
qingfeng19491001 <3543276803@qq.com>
2026-08-03 23:32:00 +0800

    chore: remove unused template code and assets

--- iosApp/iosApp/KuiklyRenderViewPage.swift
+++ iosApp/iosApp/KuiklyRenderViewPage.swift
@@ -4,7 +4,6 @@ import SwiftUI
 struct KuiklyRenderViewPage : UIViewControllerRepresentable {
     var pageName: String
     var data: Dictionary<String, Any>
-  //  typealiaUIViewControllerType = UINavigationController
     func makeUIViewController(context: Context) -> UINavigationController {
         let hrVC = KuiklyRenderViewController(pageName: pageName, pageData: data)
         return UINavigationController.init(rootViewController: hrVC)
--- shared/src/commonMain/kotlin/com/kuikly/kuiklychart/base/IPagerIdKtx.kt
+++ shared/src/commonMain/kotlin/com/kuikly/kuiklychart/base/IPagerIdKtx.kt
@@ -2,16 +2,6 @@ package com.kuikly.kuiklychart.base
 
 import com.tencent.kuikly.core.base.PagerScope
 
-/**
- * 老的方式:，需要显式传递 pagerId
- * ```kotlin
- * Utils.bridgeModule(pagerId).toast("message")
- * ```
- *
- * 新方式：无需显式传递 pagerId
- * ```kotlin
- * bridgeModule.toast("message")
- * ```
- */
+/** 无需显式传递 pagerId 即可访问当前页面的桥接模块。 */
 internal val PagerScope.bridgeModule: BridgeModule
     get() = getPager().acquireModule(BridgeModule.MODULE_NAME)
